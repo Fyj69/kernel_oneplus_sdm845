169e4bc5fc1927f172597c646f26d6ba360fe3b3
Fyj69 <18164452+Fyj69@users.noreply.github.com>
2025-04-21 22:31:29 +0800

    Synchronize code from KJ-Network

--- arch/x86/include/asm/cacheflush.h
+++ arch/x86/include/asm/cacheflush.h
@@ -3,6 +3,7 @@
 
 /* Caches aren't brain-dead on the intel. */
 #include <asm-generic/cacheflush.h>
+#include <asm/set_memory.h>
 #include <asm/special_insns.h>
 void clflush_cache_range(void *addr, unsigned int size);
 
