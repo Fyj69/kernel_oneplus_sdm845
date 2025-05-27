3e5ba99faf4d6f2961945900bdb1f2216df1b384
Fyj69 <18164452+Fyj69@users.noreply.github.com>
2025-05-27 15:34:05 +0800

    Revert to the previous commit

--- scripts/kconfig/symbol.c
+++ scripts/kconfig/symbol.c
@@ -1071,17 +1071,12 @@ static struct dep_stack {
 
 static void dep_stack_insert(struct dep_stack *stack, struct symbol *sym)
 {
-    struct dep_stack *new_stack = malloc(sizeof(*stack));
-    if (!new_stack) {
-        fprintf(stderr, "Memory allocation failed!\n");
-        return;
-    }
-    memset(new_stack, 0, sizeof(*new_stack));
-    if (check_top)
-        check_top->next = new_stack;
-    new_stack->prev = check_top;
-    new_stack->sym = sym;
-    check_top = new_stack;
+	memset(stack, 0, sizeof(*stack));
+	if (check_top)
+		check_top->next = stack;
+	stack->prev = check_top;
+	stack->sym = sym;
+	check_top = stack;
 }
 
 static void dep_stack_remove(void)
